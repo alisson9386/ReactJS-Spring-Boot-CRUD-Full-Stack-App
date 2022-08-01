9f50f99fcfec9e661acbd8b7045d18b154a827b7
Alisson Deives <alissondeives70@gmail.com>
2022-08-01 01:28:53 -0300

    Alterando classes controller, passando as regras de negocios para a classe service

--- springboot-backend/src/main/java/net/javaguides/springboot/controller/UsuarioController.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/controller/UsuarioController.java
@@ -1,6 +1,5 @@
 package net.javaguides.springboot.controller;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,9 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import net.javaguides.springboot.exception.ResourceNotFoundException;
 import net.javaguides.springboot.model.Usuario;
-import net.javaguides.springboot.repository.UsuarioRepository;
+import net.javaguides.springboot.service.UsuarioService;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -26,48 +24,32 @@ import net.javaguides.springboot.repository.UsuarioRepository;
 public class UsuarioController {
 
 	@Autowired
-	private UsuarioRepository usuarioRepository;
+	private UsuarioService usuarioService;
 	
 	@GetMapping("/usuarios")
 	public List<Usuario> listaTodos(){
-		return usuarioRepository.findAll();
+		return usuarioService.listaTodos();
 	}		
 	
 	@PostMapping("/usuarios")
 	public Usuario criarUsuario(@RequestBody Usuario usuario) {
-		return usuarioRepository.save(usuario);
+		usuarioService.criarUsuario(usuario);
+		return usuarioService.criarUsuario(usuario);
 	}
 	
 	@GetMapping("/usuarios/{id}")
 	public ResponseEntity<Usuario> buscaUsuarioPorId(@PathVariable Long id) {
-		Usuario usuario = usuarioRepository.findById(id)
-				.orElseThrow(() -> new ResourceNotFoundException("Não existe usuario com o id :" + id));
-		return ResponseEntity.ok(usuario);
+		return usuarioService.buscaUsuarioPorId(id);
 	}
 	
 	@PutMapping("/usuarios/{id}")
 	public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Long id, @RequestBody Usuario usuarioDetalhes){
-		Usuario usuario = usuarioRepository.findById(id)
-				.orElseThrow(() -> new ResourceNotFoundException("Não existe usuario com o id :" + id));
-		
-		usuario.setNome(usuarioDetalhes.getNome());
-		usuario.setIdade(usuarioDetalhes.getIdade());
-		usuario.setSkill(usuarioDetalhes.getSkill());
-		
-		Usuario atualizarUsuario = usuarioRepository.save(usuario);
-		return ResponseEntity.ok(atualizarUsuario);
+		return usuarioService.atualizarUsuario(id, usuarioDetalhes);
 	}
 	
 	@DeleteMapping("/usuarios/{id}")
 	public ResponseEntity<Map<String, Boolean>> deletarUsuario(@PathVariable Long id){
-		Usuario usuario = usuarioRepository.findById(id)
-				.orElseThrow(() -> new ResourceNotFoundException("Não existe usuario com o id :" + id));
-		
-		usuarioRepository.delete(usuario);
-		Map<String, Boolean> response = new HashMap<>();
-		response.put("deletado", Boolean.TRUE);
-		return ResponseEntity.ok(response);
+		return usuarioService.deletarUsuario(id);
 	}
-	
 	
 }
